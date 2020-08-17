9dae48acfb786ffa18c79991dd6ea77f333b4157
Vardaan Sharma <gokuldh@gmail.com>
2020-08-17 22:35:31 +0530

    Day 6
    Templates Extended
    Extending Dynamic Pages

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+
 ]
 
+## localhost/post/1
+## localhost/post/2
+## localhost/post/3
+## localhost/post/4
